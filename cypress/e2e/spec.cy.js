fb9b998dafcc7cd547f69217ed872e60615fb60f
hedizita <kovbanhezi@gmail.com>
2023-08-11 06:52:38 +0200

    made changes

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,15 +1,46 @@
-describe('Butopea Website Tests', () => {
-  it('should load the website', () => {
-    cy.visit('https://butopea.com')
-    cy.title().should('include', 'Butopea')
-  })
+//many failed tests because of these issues
+Cypress.on('uncaught:exception', (err, runnable) => {
+  if (err.message.includes('fb_api_track_info')) {
+    return false;
+  }
+  return true;
+});
 
-  it('should check for the existence of a specific element', () => {
-    cy.visit('https://butopea.com')
-    cy.get('.your-element-selector').should('exist')
-    
-    cy.get('.your-element-selector').invoke('text').then((text) => {
-      cy.log('Extracted data:', text)
-    })
-  })
-})
+describe('Butopea Website Tests', () => {
+  beforeEach(() => {
+    cy.visit('https://butopea.com');
+  });
+
+  it('should check middle square content', () => {
+    cy.get('.banner-square-overlay-heading').should('exist').invoke('text').then((text) => {
+      cy.log('Extracted middle square text:', text);
+    });
+
+    cy.get('.banner-square-overlay-button').should('exist').invoke('text').then((buttonText) => {
+      cy.log('Extracted middle square button text:', buttonText);
+    });
+  });
+
+  it('should check third square image', () => {
+    cy.get('.banner-square:nth-child(3) .banner-square-image').should('exist').invoke('attr', 'src').then((src) => {
+      cy.log('Extracted third square image URL:', src);
+    });
+  });
+
+  it('should load and check products on next tab', () => {
+    cy.get('.tabContent .tab.inactive').eq(1).click(); //kategórák tab
+    cy.wait(5000);
+
+    cy.get('.product-item').should('exist').each((product) => {
+      const title = product.find('.product-title').text();
+      const link = product.find('.product-link').attr('href');
+      const imageSrc = product.find('.product-image img').attr('src');
+      const price = product.find('.product-price').text();
+
+      cy.log('Product Title:', title);
+      cy.log('Product Link:', link);
+      cy.log('Product Image URL:', imageSrc);
+      cy.log('Product Price:', price);
+    });
+  });
+});
