54598f0674f73fcc51912be0f7753f2cdcb3fe47
hedizita <kovbanhezi@gmail.com>
2023-08-11 05:58:06 +0200

    installed mocha

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,15 +1,15 @@
 describe('Butopea Website Tests', () => {
   it('should load the website', () => {
     cy.visit('https://butopea.com')
+    cy.title().should('include', 'Butopea')
   })
-})
 
-it('should check for the existence of a specific element', () => {
-  cy.visit('https://butopea.com')
-  cy.get('.your-element-selector').should('exist')
-  
-  // Extract data and log it
-  cy.get('.your-element-selector').invoke('text').then((text) => {
-    cy.log('Extracted data:', text)
+  it('should check for the existence of a specific element', () => {
+    cy.visit('https://butopea.com')
+    cy.get('.your-element-selector').should('exist')
+    
+    cy.get('.your-element-selector').invoke('text').then((text) => {
+      cy.log('Extracted data:', text)
+    })
   })
 })
