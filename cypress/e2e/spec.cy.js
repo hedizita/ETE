9bb1350cc29734f38120e3f8a8a7e2951d607f14
hedizita <kovbanhezi@gmail.com>
2023-08-12 19:56:56 +0200

    committed

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -35,7 +35,7 @@ describe('Butopea Website Tests', () => {
 //document.querySelector("#home-content > div:nth-child(3) > div > div > div:nth-child(3) > div > a > div > div > img")
 
   //test 3
-  it('should load and check products on next tab', () => {
+  /*it('should load and check products on next tab', () => {
     cy.get('document.querySelector("#category > div.container.px0.pb30 > div > div > div > div:nth-child(1) > div > a > div.product-cover.bg-cl-secondary.product-cover-min-height > div.v-lazy-component.loaded > div > img.preview-img-item.product-image__thumb")').click();
 
 
@@ -50,5 +50,24 @@ describe('Butopea Website Tests', () => {
       cy.log('Product Image URL:', imageSrc);
       cy.log('Product Price:', price);
     });
+  });*/
+
+  it('should load and check products on next tab', () => {
+    // Click on the desired element to load the next tab
+    cy.get('#category > div.container.px0.pb30 > div > div > div > div:nth-child(1) > div > a').click();
+  
+    // Assuming .selector is the class that identifies the product items
+    cy.get('.selector', { timeout: 10000 }).should('be.visible').each((product) => {
+      const title = product.find('.product-title').text();
+      const link = product.find('.product-link').attr('href');
+      const imageSrc = product.find('.product-image img').attr('src');
+      const price = product.find('.product-price').text();
+  
+      cy.log('Product Title:', title);
+      cy.log('Product Link:', link);
+      cy.log('Product Image URL:', imageSrc);
+      cy.log('Product Price:', price);
+    });
   });
+  
 });
