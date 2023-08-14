b6f05f47a5cace547975fc66355764140cd6b0da
hedizita <kovbanhezi@gmail.com>
2023-08-14 23:24:49 +0200

    renamed file and commented

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,11 +1,9 @@
-//many failed tests because of this - configures Cypress to handle uncaught expectations
-//INNENTŐL JÓ
+//Many failed tests because of this - configures Cypress to handle uncaught expectations
 Cypress.on('uncaught:exception', (err, runnable) => {
-  //prevents Cypress for failing tests if 'fb_api_track_info' is included
-  if (err.message.includes('fb_api_track_info')) {
+  if (err.message.includes('fb_api_track_info')) { //Prevents Cypress for failing tests if 'fb_api_track_info' is included
     return false;
   }
-  //if error is not related to 'fb_api_track_info' Cypress handles it as usual
+  //If error is not related to 'fb_api_track_info' Cypress handles it as usual
   return true;
 });
 
@@ -13,16 +11,17 @@ describe('Butopea Website Tests', () => {
   beforeEach(() => {
     cy.visit('https://butopea.com');
   });
-  //test 1
+
+//--------------------------
+  //test 1 - SUCCESFUL
   it('should check middle square content', () => {
-    //gets an element with the class 'banner-square-overlay-heading' and wait for a maximum of 10 seconds for it to exist
+    //Gets an element with the class 'banner-square-overlay-heading' and waits for a maximum of 10 seconds for it to exist
     cy.get('.banner-square-overlay-heading', { timeout: 10000 }).should('exist').invoke('text').then((text) => {
       cy.log('extracted middle square text:', text);
     });
   });
-//IDÁIG JÓ
-
-//test 2
+//----------------------------
+//test 2 - SUCCESFUL
   it('should check if the right square contains an image', () => {
     cy.get('#home-content > div:nth-child(3) > div > div > div:nth-child(3) > div > a > div > div > img', { timeout: 10000 })
       .should('exist')
@@ -31,31 +30,13 @@ describe('Butopea Website Tests', () => {
         cy.log('Extracted right square image:', imageSrc);
       });
   });
-
-//document.querySelector("#home-content > div:nth-child(3) > div > div > div:nth-child(3) > div > a > div > div > img")
-
-  //test 3
-  /*it('should load and check products on next tab', () => {
-    cy.get('document.querySelector("#category > div.container.px0.pb30 > div > div > div > div:nth-child(1) > div > a > div.product-cover.bg-cl-secondary.product-cover-min-height > div.v-lazy-component.loaded > div > img.preview-img-item.product-image__thumb")').click();
-
-
-    cy.get('.selector', { timeout: 10000 }).should('be.visible').each((product) => {
-      const title = product.find('.product-title').text();
-      const link = product.find('.product-link').attr('href');
-      const imageSrc = product.find('.product-image img').attr('src');
-      const price = product.find('.product-price').text();
-
-      cy.log('Product Title:', title);
-      cy.log('Product Link:', link);
-      cy.log('Product Image URL:', imageSrc);
-      cy.log('Product Price:', price);
-    });
-  });*/
-
+//----------------------------
+  //test 3 - UNSUCCESFUL
   it('should load and check products on next tab', () => {
     // Click on the desired element to load the next tab
     //cy.get("#new-arrivals > section > div > div > div:nth-child(4) > div > a > div.product-cover.bg-cl-secondary.product-cover-min-height > div.product-image.product-cover__thumb.original-thumbnail.product-image--height > img.preview-img-item.product-image__thumb").click();
       cy.get("#category > div.container.px0.pb30 > div > div > div > div:nth-child(1) > div > a > div.product-cover.bg-cl-secondary.product-cover-min-height > div.v-lazy-component.loaded > div > img.preview-img-item.product-image__thumb").click();
+
   //'#category > div.container.px0.pb30 > div > div > div > div:nth-child(1) > div > a'
     // Assuming .selector is the class that identifies the product items
     cy.get('.selector', { timeout: 10000 }).should('be.visible').each((product) => {
@@ -64,6 +45,7 @@ describe('Butopea Website Tests', () => {
       const imageSrc = product.find('.product-image img').attr('src');
       const price = product.find('.product-price').text();
   
+      //Logged extract data
       cy.log('Product Title:', title);
       cy.log('Product Link:', link);
       cy.log('Product Image URL:', imageSrc);
